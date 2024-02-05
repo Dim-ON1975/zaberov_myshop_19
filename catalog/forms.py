75b55ca2b916c73733a32efcd25d2e45b344704f
zaberov.dv <zaberov.dv@internet.ru>
2024-02-05 19:38:58 +0300

    Не запускаеют методы clean_ для формы.

--- catalog/forms.py
+++ catalog/forms.py
@@ -3,8 +3,22 @@ from .models import Product
 
 
 class ProductForm(forms.ModelForm):
+    FORBIDDEN = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар']
+
     class Meta:
         model = Product
-        fields = '__all__'
+        fields = ('name', 'description', 'img', 'category', 'price', 'is_active',)
 
+    def clean_name(self):
+        cleaned_name = self.cleaned_data['name']
+        for s in self.FORBIDDEN:
+            if s.lower() in cleaned_name.lower():
+                raise forms.ValidationError('Имя товара содержит недопустимые данные')
+        return cleaned_name
 
+    def clean_description(self):
+        cleaned_description = self.cleaned_data['description']
+        for s in self.FORBIDDEN:
+            if s.lower() in cleaned_description.lower():
+                raise forms.ValidationError('Описание товара содержит недопустимые данные')
+        return cleaned_description
